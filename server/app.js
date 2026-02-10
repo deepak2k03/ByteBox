ecd3a5ea444696b1bf5e628625804eec3c038254
Deepak <sman59472@gmail.com>
2026-02-10 09:34:34 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,64 @@
+import express from "express";
+import { createWriteStream } from "fs";
+import { readdir } from "fs/promises";
+import { rm } from "fs/promises";
+import { rename } from "fs/promises";
+import cors from "cors";
+
+const app = express();
+
+//json parsing
+app.use(express.json())
+
+//enabling cors
+app.use(cors())
+
+//Read
+app.get("/directory", async (req, res) => {
+  const filesList = await readdir("./storage");
+  res.json(filesList);
+});
+
+
+//create 
+app.post("/files/:filename",(req,res)=>{
+  console.log(req.params.filename);
+  const writeStream=createWriteStream(`./storage/${req.params.filename}`);
+  req.pipe(writeStream);
+  req.on("end",()=>{
+    res.json({message:"File Uploaded"});
+  })
+})
+
+
+app.get("/files/:filename", (req, res) => {
+  const { filename } = req.params;
+  if (req.query.action === "download") {
+    res.set("Content-Disposition", "attachment");
+  }
+  res.sendFile(`${import.meta.dirname}/storage/${filename}`);
+});
+
+//update 
+app.patch("/files:filename",async(req,res)=>{
+  const {filename}=req.params;
+  await rename(`./storage/${filename}`,`./storage/${req.body.newFilename}`);
+  res.json({message:"Renamed"});
+})
+
+//delete
+app.delete("/files/:filename", async (req, res) => {
+  const { filename } = req.params;
+  const filePath=`./storage/${filename}`;
+  try {
+    await rm(filePath);
+    res.json({message:"File Deleted"});
+  } catch (error) {
+    res.status(404).json({message:"File Not Found"});
+  }
+});
+
+
+app.listen(4000, () => {
+  console.log(`Server Started`);
+});
